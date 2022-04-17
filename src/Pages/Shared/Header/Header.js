ff95404ffac5c86e963e56d645bfc5bbff1c926d
fahad09fahim <anik.fahim07@gmail.com>
2022-04-17 10:36:47 +0600

    first commit

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        sticky="top"
+      >
+        <Container>
+          <Navbar.Brand href="#home">Sports Flash</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">Services</Nav.Link>
+              <Nav.Link href="#pricing">Blogs</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Log in</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
