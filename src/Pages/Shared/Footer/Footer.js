8ece5bac517bb7eecadb783a9b4847608edbe9f8
fahad09fahim <anik.fahim07@gmail.com>
2022-04-17 16:58:08 +0600

    footer complete

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,40 @@
 import React from "react";
+import "./Footer.css";
+import { FaFacebook, FaGithub, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
+  const today = new Date();
+  const year = today.getFullYear();
   return (
-    <div>
-      <h1>this is footer</h1>
+    <div className="footer">
+      <hr />
+      <footer>
+        <div className="social">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.facebook.com/fahad.fahim09"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/fahad09fahim"
+          >
+            <FaGithub />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.instagram.com/09_fahim/"
+          >
+            <FaInstagram />
+          </a>
+        </div>
+
+        <p className="copyright">Sports Flash © {year}</p>
+      </footer>
     </div>
   );
 };
